ac0555bf5f61c169041fe606cf2615a31ee2a7d7
sumoh001 <umoh.simon@yahoo.de>
2024-05-25 15:29:30 +0200

    anpassung

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
@@ -1,6 +1,6 @@
 package ch.zhaw.iwi.devops.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +10,7 @@ public class ToDoControllerTest {
     public void testCreate() {
         ToDoController controller = new ToDoController();
         var todo = new ToDo(1, "t", "d");
-        controller.createTodo(1, todo);
+        controller.createTodo(2, todo);
         assertEquals(1, controller.count());
         assertEquals(1, controller.todo().size());
     }
